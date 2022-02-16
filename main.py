d08cbe5b9d4475804f77f67b2197ae2d23b57d9b
MuzAnSka <MuzAnSka@yandex.ru>
2022-02-16 11:09:00 +0400

    Создание библиотек

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import argparse
+from matplotlib import pyplot as plt
+from skimage.io import imread, imshow, show, imsave
+from skimage.transform import resize
+from skimage.exposure import histogram
